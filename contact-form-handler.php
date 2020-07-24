8eab11e0ffe5ea1a19045b13154251d3f9fe8ef3
Tree-lance <49657774+Tree-lance@users.noreply.github.com>
2020-07-24 15:34:41 +0100

    1st draft

---
+++ contact-form-handler.php
@@ -0,0 +1,25 @@
+<?php
+name = _POST ['name'];
+visitor_email = _POST['email'];
+message = _POST['message'];
+
+
+email_from = 'knowledgeiskey@mail.com';
+
+email_subject = "New Form Submission";
+
+email_body = "User Name: $name\n".
+                  "User Email: $visitor_email.\n".
+                      "User Message: $message.\n";
+
+to = "knowledgeiskey@mail.com";
+
+headers = "From: $email_from \r\n";
+
+headers = "Reply-To: $visitor_email \r\n";
+
+mail(to, email_subject, email_body, message, headers);   
+
+header("location: index.html");                   
+
+?>
